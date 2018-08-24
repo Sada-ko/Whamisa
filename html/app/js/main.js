5cbe1757381a53d184cc155069b7e2fe844a0270
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2018-08-24 19:44:51 +0300

    Скрипты с табами

--- html/app/js/main.js
+++ html/app/js/main.js
@@ -9,8 +9,8 @@
 //= ../../node_modules/bootstrap/js/dist/util.js
 //= ../../node_modules/bootstrap/js/dist/alert.js
 //= ../../node_modules/bootstrap/js/dist/dropdown.js
-//= ../../node_modules/bootstrap/js/dist/button.js
 //= ../../node_modules/bootstrap/js/dist/modal.js
+//= ../../node_modules/bootstrap/js/dist/tab.js
 //= ../../node_modules/bootstrap/js/dist/collapse.js
 
 // OWL.CAROUSEL
