83858e9d84a0b87f0de55cf152d2980b14770bc8
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2018-08-27 18:23:11 +0300

    Списки и валидация форм

--- html/app/js/app.js
+++ html/app/js/app.js
@@ -65,3 +65,25 @@ $('.owl-carousel.main-carousel').owlCarousel({
 	autoplayTimeout:20000,
 	items:1,
 })
+
+// FORM VALIDATION
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+$(document).ready(function () {
+	(function() {
+		'use strict';
+		window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+    	form.addEventListener('submit', function(event) {
+    		if (form.checkValidity() === false) {
+    			event.preventDefault();
+    			event.stopPropagation();
+    		}
+    		form.classList.add('was-validated');
+    	}, false);
+    });
+}, false);
+	})();
+});
